34efb84900cf6431e429a2a0642ece46c95b3d9b
Sagar Thalkiya <88761737+SagarThalkiya@users.noreply.github.com>
2024-07-22 20:19:43 +0530

    Time: 13 ms (53.71%), Space: 14.6 MB (12.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //takin size of the array
-       int n = nums.size();
-
-    //declaring vector since it is dynamic
-    vector<int> temp ;
-
-    //first loop welli run anothr loop n-1 times
-    for(int i=0;i<n-1;i++){   
-        //second loop will run until its over then agian first loop will start
-        for(int j=i+1;j<n;j++){
-            if(nums[i]+nums[j]==target){
-                //stores index of numbers in vector
-                temp.push_back(i);
-                temp.push_back(j);
+        int n = nums.size();
+        
+        map<int, int> mpp;
+        for(int i=0;i<n;i++){
+            int remaining = target - nums[i];
+            if(mpp.find(remaining) != mpp.end()){
+                
+                return {mpp[remaining],i};
             }
+            mpp[nums[i]] = i;
         }
-    }
-    //displays the vector temp
-    return temp;
-       
-
-}
+        
+        return {};
+    }    
 };

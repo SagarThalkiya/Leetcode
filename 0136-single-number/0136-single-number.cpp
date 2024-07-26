5b7080b5960f35ab671d662f3e36414f766b3650
Sagar Thalkiya <88761737+SagarThalkiya@users.noreply.github.com>
2024-07-26 15:52:18 +0530

    Time: 21 ms (21.33%), Space: 20.7 MB (18.52%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans =0;
+        sort(nums.begin(),nums.end());
+        for(int i =0;i<n;i++){
+            ans = ans ^ nums[i];
+        }
+        
+        
+        
+        
+        
+//         map<int,int> mpp;
+//         for(int i =0;i<n;i++){
+
+//           mpp[nums[i]]++;
+//         }
+        
+//         for(auto it : mpp){
+
+//             if(it.first < 2){
+//                 return it.second;
+//             }
+//         }
+        
+        return ans;
+    }  
+};

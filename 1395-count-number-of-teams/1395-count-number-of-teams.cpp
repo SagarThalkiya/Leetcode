2bf059ae2709b3e60491689c4aef48981e4d6cd2
Sagar Thalkiya <88761737+SagarThalkiya@users.noreply.github.com>
2024-07-29 15:51:07 +0530

    Time: 25 ms (86.30%), Space: 11.7 MB (51.74%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,18 +1,46 @@
+// class Solution {
+// public:
+//     int numTeams(vector<int>& rating) {
+//         int cnt = 0;
+//         int n = rating.size();
+//         for(int i = 0; i < n - 2; ++i) {
+//             for(int j = i + 1; j < n - 1; ++j) {
+//                 for(int k = j + 1; k < n; ++k) {
+//                     if ((rating[i] < rating[j] && rating[j] < rating[k]) || 
+//                         (rating[i] > rating[j] && rating[j] > rating[k])) {
+//                         cnt++;
+//                     }
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int cnt = 0;
         int n = rating.size();
-        for(int i = 0; i < n - 2; ++i) {
-            for(int j = i + 1; j < n - 1; ++j) {
-                for(int k = j + 1; k < n; ++k) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) || 
-                        (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        cnt++;
-                    }
-                }
+        
+        for (int j = 1; j < n - 1; ++j) {
+            int less_left = 0, greater_left = 0;
+            int less_right = 0, greater_right = 0;
+            
+            for (int i = 0; i < j; ++i) {
+                if (rating[i] < rating[j]) less_left++;
+                if (rating[i] > rating[j]) greater_left++;
             }
+            
+            for (int k = j + 1; k < n; ++k) {
+                if (rating[k] < rating[j]) less_right++;
+                if (rating[k] > rating[j]) greater_right++;
+            }
+            
+            cnt += less_left * greater_right + greater_left * less_right;
         }
+        
         return cnt;
     }
 };
+

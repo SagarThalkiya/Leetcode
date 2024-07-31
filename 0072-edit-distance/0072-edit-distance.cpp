ef815003683a5ab456794613013c4dd7d91ef544
Sagar Thalkiya <88761737+SagarThalkiya@users.noreply.github.com>
2024-07-31 17:27:18 +0530

    Time: 0 ms (100.00%), Space: 12.8 MB (33.32%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        // Create a (m+1) x (n+1) DP table
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        
+        // Initialize the base cases
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = i; // Deleting all characters of word1
+        }
+        for (int j = 0; j <= n; ++j) {
+            dp[0][j] = j; // Inserting all characters of word2
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min({dp[i - 1][j] + 1,    // Delete
+                                    dp[i][j - 1] + 1,    // Insert
+                                    dp[i - 1][j - 1] + 1 // Replace
+                                   });
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
